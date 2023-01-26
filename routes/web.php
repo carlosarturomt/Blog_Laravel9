8242239340d03d1d30e1a7338e24693ddf6aa8f9
Carlos Arturo MT <carlosarturomt@gmail.com>
2023-01-25 23:44:44 -0600

    login system

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PostController;
 
 Route::controller(PageController::class)->group(function() {
 
@@ -10,3 +11,19 @@ Route::controller(PageController::class)->group(function() {
     Route::get('blog/{post:slug}', 'post')->name('post');
 
 });
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+Route::resource('posts', PostController::class)->except(['show']);
+
+require __DIR__.'/auth.php';
